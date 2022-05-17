78ea946857035a76b1346e8a48ac636b500f1247
wfrazier002 <wbf0007@tigermail.auburn.edu>
2022-05-17 08:57:17 -0400

    adding files/folders pt15

---
+++ Wpf_Practice14_triggers_data_props_events/Wpf_Practice14_triggers_data_props_events/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_Practice14_triggers_data_props_events {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
